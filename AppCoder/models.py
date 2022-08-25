31faf723dc9bb4b896cf01077f997b667293ee7b
FrancoDiMartino <francogdimartino@gmail.com>
2022-08-25 20:37:14 -0300

    fin clase 20

--- AppCoder/models.py
+++ AppCoder/models.py
@@ -5,6 +5,12 @@ from django.db import models
 class Curso(models.Model):
     nombre=models.CharField(max_length=50)
     comision=models.IntegerField()
+    
+
+    def __str__(self):
+        return self.nombre+" "+str(self.comision)
+
+
 
 class Estudiante(models.Model):
     nombre= models.CharField(max_length=50)
--- AppCoder/views.py
+++ AppCoder/views.py
@@ -7,19 +7,32 @@ from .models import Curso
 def curso(request):
     
     curso=Curso(nombre="Curso de Python",comision=123456)
+    
     curso.save()
+   
     texto=f"Curso Creado: nombre: {curso.nombre} comision: {curso.comision}"
     return HttpResponse(texto)
+
+
+
 
 
 def inicio(request):
     return render (request, "Appcoder/inicio.html")
 
 def cursos(request):
-    return render(request, "Appcoder/cursos.html")
+    curso1=Curso(nombre="Cursito de python",comision=31100)
+    curso1.save()
+    curso2=Curso(nombre="Cursito de django",comision=31101)
+    curso2.save()
+    lista=[curso1, curso2]
+    return render(request, "Appcoder/cursos.html", {"listita":lista})
 
 def profesores(request):
-    return render(request, "Appcoder/profesores.html")
+
+    
+
+    return render(request, "Appcoder/profesores.html" )
 
 def estudiantes(request):
     return render(request, "Appcoder/estudiantes.html")
